b57b8492c08749c9f5579305e841e902d7ab2607
Pablo <pablo.aguerre@gmail.com>
2014-11-04 03:13:06 -0300

    Revert "rest client update to internal IP"
    This reverts commit b0fe4a211c3f7861f445006694e1e35bc718d6c7.

--- rh-integral-consulting/public/js/custom/rest-client.js
+++ rh-integral-consulting/public/js/custom/rest-client.js
@@ -1,4 +1,4 @@
-var SERVER_URI = 'http://10.240.167.82:8080/',
+var SERVER_URI = 'http://localhost:8888/',
 OFFERS_TYPE = 'offers',
 JOB_APPLICATIONS_TYPE = 'job-applications',
 MESSAGES_TYPE = 'messages',
