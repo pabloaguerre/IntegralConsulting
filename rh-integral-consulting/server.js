f1a019713d0351814162e94bde75d1ccf8f3c05a
Pablo <pablo.aguerre@gmail.com>
2014-11-06 02:05:23 -0300

    CORS support

--- rh-integral-consulting/server.js
+++ rh-integral-consulting/server.js
@@ -6,26 +6,6 @@ var express = require('express'),
  
 var app = express();
 
-// Add headers
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://www.rhintegralconsulting.com');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    //res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
-
 app.configure(function () {
 	app.set('port', process.env.PORT || 80);
 	app.use(express.logger('dev')); // 'default', 'short', 'tiny', 'dev' 
@@ -33,6 +13,26 @@ app.configure(function () {
 	app.use(express.urlencoded({limit: JSON_LIMIT}));
 	app.use(express.bodyParser());
     app.use(express.static(path.join(__dirname, 'public')));
+
+    // Add headers
+	app.use(function (req, res, next) {
+
+	    // Website you wish to allow to connect
+	    res.setHeader('Access-Control-Allow-Origin', 'http://www.rhintegralconsulting.com');
+
+	    // Request methods you wish to allow
+	    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+	    // Request headers you wish to allow
+	    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
+
+	    // Set to true if you need the website to include cookies in the requests sent
+	    // to the API (e.g. in case you use sessions)
+	    res.setHeader('Access-Control-Allow-Credentials', false);
+
+	    // Pass to next layer of middleware
+	    next();
+	});
 });
 
 app.get('/offers', dbMngr.findOffers);
