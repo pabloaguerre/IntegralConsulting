337d18be96288c75997e77612703260b027de351
Pablo <pablo.aguerre@gmail.com>
2014-11-06 01:51:13 -0300

    CORS support
    Enabled CORs support

--- rh-integral-consulting/server.js
+++ rh-integral-consulting/server.js
@@ -6,6 +6,26 @@ var express = require('express'),
  
 var app = express();
 
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://www.rhintegralconsulting.com');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    //res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.configure(function () {
 	app.set('port', process.env.PORT || 80);
 	app.use(express.logger('dev')); // 'default', 'short', 'tiny', 'dev' 
